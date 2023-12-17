712eebd552ed1c290269f50dee08ec6d7f7c9d0b
Maximkalenkovich <maxim.peterson@mail.ru>
2023-12-17 15:55:07 +0300

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,7 @@ import React from "react";
 export const Header = () =>{
     return(
         <header className='header'>
-
                <img src = {Logo} alt = 'logo'/>
-
-
-
         </header>
     )
 }
