56af9202abac04067578ace63e02ece787fd2101
Maximkalenkovich <maxim.peterson@mail.ru>
2023-12-21 20:22:43 +0300

    add div

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -17,15 +17,7 @@ export const Profile = () => {
             <div className={s.item}>post 1</div>
             <div className={s.item}>post 2</div>
             <div className={s.item}>post 3</div>
-            <div className={s.item}>post 1</div>
-            <div className={s.item}>post 2</div>
-            <div className={s.item}>post 3</div>
-            <div className={s.item}>post 1</div>
-            <div className={s.item}>post 2</div>
-            <div className={s.item}>post 3</div>
-            <div className={s.item}>post 1</div>
-            <div className={s.item}>post 2</div>
-            <div className={s.item}>post 3</div>
+
         </div>
     )
 }
